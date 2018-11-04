e59a77f29c37dc9d524f68db739689bc1499a830
zhouy546 <zhouyunqi546@gmail.com>
2018-11-04 14:20:22 +0800

    Initial commit

---
+++ Assets/Script/CreateGrid.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+public class CreateGrid : MonoBehaviour {
+    public int width = 10;
+    public int depth = 10;
+    public GameObject cubePrefab;
+    public Transform parent;
+    // Use this for initialization
+    void Start() {
+        initialization();
+    }
+
+    public void initialization(){
+        CreateObjectGrid(cubePrefab);
+    }
+
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    public void CreateObjectGrid(GameObject gameObject) {
+        var EntityManager = World.Active.GetOrCreateManager<EntityManager>();
+        NativeArray<Entity> cubeArray = new NativeArray<Entity>(width * depth, Allocator.Temp);
+
+        int i = 0;
+       EntityManager.Instantiate(cubePrefab, cubeArray);
+
+        for (int x  = -width/2; x < width/2; x++)
+        {
+            for (int z = -width/2; z < depth/2; z++)
+            {
+                EntityManager.AddComponentData(cubeArray[i], new ICD_MoveHeight { dis = Random.Range(0.1f, 0.5f) });
+                EntityManager.AddComponentData(cubeArray[i], new ICD_MoveSpeed { MoveSpeed = Random.Range(0.01F, 0.09F) });
+                float3 startPos = new float3(x, 0, z);
+                EntityManager.SetComponentData(cubeArray[i], new Position { Value = startPos });
+
+
+
+              //  Debug.Log(i);
+                i++;
+            }
+        }
+
+        cubeArray.Dispose();
+
+      
+    }
+}
---
+++ Assets/Script/MovementJob.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Jobs;
+using Unity.Entities;
+using Unity.Transforms;
+
+
+public struct ICD_MoveSpeed : IComponentData {
+    public float MoveSpeed;
+}
+
+public struct ICD_MoveHeight : IComponentData {
+    public float dis;
+}
+
+public struct ICD_YDirection : IComponentData {
+
+}
+
+public class MovementJob : JobComponentSystem {
+    private struct moveJob : IJobProcessComponentData<ICD_MoveHeight, ICD_MoveSpeed, Position>
+    {
+
+        public float DeltaTime;
+     
+        public void Execute(ref ICD_MoveHeight MoveHieght, ref ICD_MoveSpeed speed, ref Position pos)
+        {
+
+
+
+                pos.Value.y += speed.MoveSpeed * DeltaTime;
+                if (pos.Value.y > MoveHieght.dis|| pos.Value.y < -MoveHieght.dis)
+                {
+                speed.MoveSpeed *= -1;
+                }
+
+        }
+    }
+
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        var Job = new moveJob
+        {
+            DeltaTime = Time.deltaTime,
+        };
+
+        return Job.Schedule(this, inputDeps);
+    }
+
+}
